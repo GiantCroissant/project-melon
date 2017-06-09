8f7741ea6726058f0fdd0e2ff5172fa339ae6ce1
ApprenticeGC <apprentice@giantcroissant.com>
2017-06-09 17:44:43 +0800

    Add functionality to load data from json file

--- app/src/stores/Overview/index.js
+++ app/src/stores/Overview/index.js
@@ -3,28 +3,15 @@ import React from 'react';
 import { observable, computed, action } from 'mobx';
 import { observer } from 'mobx-react';
 
+//
+import data from './data.json';
+
 export default class {
   @observable
   overviews = [];
 
   constructor() {
-    this.setOverviews([
-      {
-        id: "f1f9acb3-5a7e-46dc-8d12-bcc4eca2dd13",
-        title: "Article 001",
-        subTitle: "Sub Title 001"
-      },
-      {
-        id: "aed638d3-5cdc-4d23-91f5-58d61530d1b1",
-        title: "Article 002",
-        subTitle: "Sub Title 002"
-      },
-      {
-        id: "e736fe94-e1a9-4356-a9d7-65992cc92cac",
-        title: "Article 003",
-        subTitle: "Sub Title 003"
-      }
-    ]);
+    this.setOverviews(data.overviews);
   }
 
   @action
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -50,6 +50,10 @@ module.exports = {
             ]
           }
         }
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader'
       }
     ]
   },
